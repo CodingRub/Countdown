91c956a16563886e5de1695cce1a7c51cef0b6b8
CodingRub <57622401+CodingRub@users.noreply.github.com>
2020-12-31 10:56:05 +0100

    Update ajax.js

--- Countdown/js/ajax.js
+++ Countdown/js/ajax.js
@@ -1,19 +1,35 @@
-function charger(){
-
-    setTimeout( function(){
-        // on lance une requête AJAX
-        $.ajax({
-            url : "charger.php",
-            type : GET,
-            success : function(html){
-                $('#messages').prepend(html); // on veut ajouter les nouveaux messages au début du bloc #messages
-            }
-        });
-
-        charger(); // on relance la fonction
-
-    }, 5000); // on exécute le chargement toutes les 5 secondes
-
+function getMessages(){
+    const requeteAjax = new XMLHttpRequest();
+    requeteAjax.open("GET", "send.php");
+    requeteAjax.onload = function() {
+        const resultat = JSON.parse(requeteAjax.responseText);
+        const html = resultat.map(function(message){
+            return `<div id="complete" style="padding: 10px; color: white">
+                        <span class="author">${message.pseudo}: </span>
+                        <span class="content">${message.chat}</span>
+                    </div>`
+        }).join('');
+        const messages = document.querySelector('.scroller');
+        messages.innerHTML = html;
+        messages.scrollTop = messages.scrollHeight;
+    }
+    requeteAjax.send();
 }
 
-charger();
+function postMessage(event){
+    event.preventDefault();
+    const content = document.querySelector("#message");
+    const data = new FormData();
+    data.append('message', content.value);
+    const requeteAjax = new XMLHttpRequest();
+    requeteAjax.open("POST", 'send.php?task=write');
+    requeteAjax.onload = function(){
+        content.value = "";
+        content.focus();
+        getMessages();
+    }
+    requeteAjax.send(data)
+
+}
+document.querySelector('form').addEventListener('submit', postMessage);
+const interval = window.setInterval(getMessages, 2000);
