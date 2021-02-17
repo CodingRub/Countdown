0649372e7a7e9a58ed9d34cf041b31cd77d90747
CodingRub <57622401+CodingRub@users.noreply.github.com>
2021-02-17 23:02:25 +0100

    Update send.php

--- Countdown/send.php
+++ Countdown/send.php
@@ -5,6 +5,7 @@ $task = "list";
 $author = $_SESSION['pseudo'];
 
 
+
 if(array_key_exists("task", $_GET)) {
     $task = $_GET['task'];
 }
@@ -25,16 +26,40 @@ function getMessage(){
 function postMessage() {
     global $author;
     global $bdd;
-
-    if(!array_key_exists('message', $_POST)){
+    $content = $_POST['message'];
+    if(empty($content)) {
         echo json_encode(["status" => "error", "message" => "Le champs du message est vide !"]);
         return;
     } else {
         $content = $_POST['message'];
-        $color = $_POST['color'];
-        $query = $bdd->prepare('INSERT INTO messages SET pseudo = :pseudo, chat = :chat, color = :color');
-        $query->execute(["pseudo" => $author, "chat" => $content, "color" => $color]);
-        echo json_encode(["status" => "success", "message" => "Le message est bien pris en compte !"]);
+        $id = $_SESSION['id'];
+        if (empty($_POST["color"])) {
+            echo("vide");
+            echo($_SESSION['color']);
+            $query = $bdd->prepare('INSERT INTO messages SET pseudo = :pseudo, statut = :statut, chat = :chat, color = :color');
+            if($_SESSION['statut'] == "admin") {
+                $query->execute(["pseudo" => $author, "statut" => "<i class='fas fa-user-shield'></i>", "chat" => $content, "color" => $_SESSION['color']]);
+            } else {
+                $query->execute(["pseudo" => $author, "statut" => "", "chat" => $content, "color" => $_SESSION['color']]);
+            }
+            echo json_encode(["status" => "success", "message" => "Le message est bien pris en compte !"]);
+        } else {
+            echo("OK");
+            $color = $_POST['color'];
+            $author = $_SESSION['pseudo'];
+            $queryColor = $bdd->prepare('UPDATE member SET color = :color WHERE id = :id');
+            $queryColor->execute(["color" => $color, "id" => $id]);
+            $queryUpColor = $bdd->prepare('UPDATE messages SET color = :color WHERE pseudo = :pseudo');
+            $queryUpColor->execute(["color" => $color, "pseudo" => $author]);
+            $_SESSION['color'] = $color;
+            $query = $bdd->prepare('INSERT INTO messages SET pseudo = :pseudo, statut = :statut, chat = :chat, color = :color');
+            if($_SESSION['statut'] == "admin") {
+                $query->execute(["pseudo" => $author, "statut" => "<i class='fas fa-user-shield'></i>", "chat" => $content, "color" => $_SESSION['color']]);
+            } else {
+                $query->execute(["pseudo" => $author, "statut" => "", "chat" => $content, "color" => $_SESSION['color']]);
+            }
+            echo json_encode(["status" => "success", "message" => "Le message est bien pris en compte !"]);
+        }
     }
 }
 ?>
